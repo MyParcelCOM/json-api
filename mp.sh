83660a228376410b540933cccdca75db510f26f4
Martin Boer <martin@myparcel.com>
2017-10-18 23:42:50 +0200

    :hammer: improved mp script

--- mp.sh
+++ mp.sh
@@ -3,9 +3,10 @@ set -eo pipefail
 
 # init environment variables
 set -o allexport
-ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-COMPOSE="docker-compose"
-DO="run --rm"
+{
+  ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+  COMPOSE="docker-compose --project-name myparcelcomcommon"
+}
 set +o allexport
 
 # run commands
@@ -13,10 +14,7 @@ if [ $# -gt 0 ]; then
   if [ -f "mp/$1" ]; then
     SCRIPT="$1"
     shift 1
-    ./mp/${SCRIPT} "$@"
-  elif [ "$1" == "help" ]; then
-    echo -e "\033[0;30;47m Available commands \033[0m"
-    ls -1 mp
+    ${ROOT_DIR}/mp/${SCRIPT} "$@"
   else
     ${COMPOSE} "$@"
   fi
