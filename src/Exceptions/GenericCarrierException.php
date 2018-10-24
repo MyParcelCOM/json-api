e4972e641d4b3b5dbd8dcbe87faf685d1ef68381
Dennis Koster <dennis@myparcel.com>
2018-10-24 13:58:21 +0200

    :sparkles: Add multi error exceptions

---
+++ src/Exceptions/GenericCarrierException.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyParcelCom\JsonApi\Exceptions;
+
+use Throwable;
+
+/**
+ * Thrown when carrier returned errors that
+ * we could not map. This is the fallback exception.
+ */
+class GenericCarrierException extends AbstractMultiErrorException
+{
+    /**
+     * @param array          $errors
+     * @param int            $status
+     * @param Throwable|null $previous
+     */
+    public function __construct(array $errors, int $status = 500, ?Throwable $previous = null)
+    {
+        parent::__construct($status, $errors, $previous);
+    }
+}
