aafcd49b5f3533303b33db61dc1bd09f261aa537
Martin Boer <martin@myparcel.com>
2021-01-19 01:47:35 +0100

    :recycle: port shipment date filter helper

--- src/Filters/Traits/AppliesFiltersTrait.php
+++ src/Filters/Traits/AppliesFiltersTrait.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace MyParcelCom\JsonApi\Filters\Traits;
 
 use Illuminate\Database\Eloquent\Builder;
+use MyParcelCom\JsonApi\Exceptions\UnprocessableEntityException;
 use MyParcelCom\JsonApi\Filters\QueryFilter;
 
 trait AppliesFiltersTrait
@@ -41,5 +42,23 @@ trait AppliesFiltersTrait
         });
 
         return $query;
+    }
+
+    /**
+     * Append time to date filters to avoid using a raw query with DATE(created_at/register_at).
+     * @param string $filter
+     * @param bool   $lastMinute
+     * @param array  $filters
+     * @throws UnprocessableEntityException
+     */
+    private function setupDateFilter(string $filter, bool $lastMinute, array &$filters): void
+    {
+        if (!empty($filters[$filter])) {
+            if (!preg_match('/^\d{4}-\d{2}-\d{2}$/', $filters[$filter])) {
+                $filterPath = str_replace('.', '][', $filter);
+                throw new UnprocessableEntityException("The filter[${filterPath}] is not in ISO 8601 date format");
+            }
+            $filters[$filter] .= $lastMinute ? ' 23:59:59' : ' 00:00:00';
+        }
     }
 }
